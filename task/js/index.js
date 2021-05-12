6d8d2d78499510475ab0feac8099f931f9091500
Gleysa Muñoz <gleysamorales@gmail.com>
2021-05-12 22:32:03 +0100

    df

--- task/js/index.js
+++ task/js/index.js
@@ -1,7 +1,7 @@
-$(document).ready(function() { setInterval(move_window, 0);});
 function move_window(){
   $("#panel_left").animate({marginLeft: "-92px"}, 1000).delay(500);
   $("#panel_right").animate({marginLeft: "91px"}, 1000).delay(500);
   $("#panel_left").animate({marginLeft: "0px"}, 1000);
   $("#panel_right").animate({marginLeft: "0px"}, 1000);
-  } 
+  requestAnimationFrame(move_window); }
+requestAnimationFrame(move_window);
